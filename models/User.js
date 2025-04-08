e446643b79b8db0193d49f90f75a9837315597a4
Jean <89807593+naejshaw@users.noreply.github.com>
2025-04-08 20:15:49 -0300

    Add models/User.js 1744154149562

---
+++ models/User.js
@@ -0,0 +1,25 @@
+const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
+
+const userSchema = new mongoose.Schema({
+  name: { type: String, required: true },
+  email: { type: String, required: true, unique: true },
+  phone: { type: String, required: true },
+  password: { type: String, required: true },
+  userType: { type: String, enum: ['client', 'professional'], required: true },
+  services: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Service' }] // For professionals only
+});
+
+// Hash password before saving
+userSchema.pre('save', async function(next) {
+  if (!this.isModified('password')) return next();
+  this.password = await bcrypt.hash(this.password, 10);
+  next();
+});
+
+// Method to compare passwords
+userSchema.methods.comparePassword = async function(candidatePassword) {
+  return await bcrypt.compare(candidatePassword, this.password);
+};
+
+module.exports = mongoose.model('User', userSchema);
