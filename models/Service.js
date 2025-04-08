e19ff53c289ca664adfb56fe6a52adf7033a6452
Jean <89807593+naejshaw@users.noreply.github.com>
2025-04-08 20:15:47 -0300

    Add models/Service.js 1744154147736

---
+++ models/Service.js
@@ -0,0 +1,34 @@
+const mongoose = require('mongoose');
+
+const serviceSchema = new mongoose.Schema({
+  professional: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: true
+  },
+  name: {
+    type: String,
+    required: true
+  },
+  description: {
+    type: String
+  },
+  duration: {
+    type: Number, // in minutes
+    required: true
+  },
+  price: {
+    type: Number,
+    required: true
+  },
+  active: {
+    type: Boolean,
+    default: true
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now
+  }
+});
+
+module.exports = mongoose.model('Service', serviceSchema);
